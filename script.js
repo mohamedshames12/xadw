cbd6e8b499abd68f6606817a9157d71a4bf959ba
mohamedshames12 <106402690+mohamedshames12@users.noreply.github.com>
2022-06-19 16:46:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var btn = document.getElementById("create");
+var dome = document.getElementById("dome")
+
+
+var hack = document.getElementById("hack");
+var caon = document.getElementById("na");
+hack.addEventListener("click" , (eo) => { 
+    
+    caon.innerHTML = "You'er Wellcom to Hacking"
+ })
+
+
+btn.addEventListener("click" , (eo) => { 
+    for(var x =1; x => 10  ; ++x ){
+        console.log(x)
+       }
+ })
+
